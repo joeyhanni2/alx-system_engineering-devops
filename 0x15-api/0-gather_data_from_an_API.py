08803036c90637045c23717f05498d4d9f34fff8
joeyhanni2 <joeyhanni@gmail.com>
2024-08-05 15:43:09 -0700

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/pythoni3
+import json
+import sys
+def get_employee_todo_list(employee ID):
+try:
+	user.response = request.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+	user = user.response.json()
+	employee_name=user(name)
+
+total_tasks = len(tasks)
+done_tasks = (for task in tasks if task ['completed']))
+num_of_done_tasks = len (done-tasks)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+	print("Usage: python script.py <employee_id>")
+else:
+	try:
+	employee_id = int(sys.argv[1])
+	get_employee_todo_progress(employee_id)
+	except ValueError:
+	print("Please provide a valid integer for the employee ID.")
+
+
+
+
+
+
+
